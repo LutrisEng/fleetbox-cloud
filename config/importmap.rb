35791a0907b115a23797b07560d36fee8763780c
Piper McCorkle <piper@lutris.engineering>
2022-06-29 19:33:57 -0500

    Use masonry for cards

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js' # @5.1.3
 pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.5/dist/esm/index.js' # @2.11.5
+pin 'masonry-layout', to: 'https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js' # @4.2.2
