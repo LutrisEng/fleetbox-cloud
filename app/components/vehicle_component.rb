458305d4cf30597bd511cb80b06f5a2ee1a4c758
Piper McCorkle <piper@lutris.engineering>
2022-06-30 01:57:01 -0500

    Don't show maintenance log on vehicle list

--- app/components/vehicle_component.rb
+++ app/components/vehicle_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class VehicleComponent < ViewComponent::Base
-  def initialize(vehicle:, current_user:, with_photo: false, with_title: true)
+  def initialize(vehicle:, current_user:, with_photo: false, with_title: true, with_log: true)
     super
     @vehicle = vehicle
     @current_user = current_user
     @with_photo = with_photo
     @with_title = with_title
+    @with_log = with_log
   end
 end
