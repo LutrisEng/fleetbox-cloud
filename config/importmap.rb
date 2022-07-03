a5d7e10adb1d35ffb32b9dadaa6d82ff5cb6f0a2
Piper McCorkle <piper@lutris.engineering>
2022-07-03 13:34:57 -0500

    Use bootstrap.js from Bootstrap gem

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,6 @@ pin 'application', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'bootstrap', to: 'https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js' # @5.1.3
-pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.5/dist/esm/index.js' # @2.11.5
-pin 'masonry-layout', to: 'https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js' # @4.2.2
+pin 'bootstrap', to: 'bootstrap.js', preload: true
+pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.5/dist/esm/index.js', preload: true # @2.11.5
+pin 'masonry-layout', to: 'https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js', preload: true # @4.2.2
