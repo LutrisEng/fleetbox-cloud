a9d7bb7a1721485c95ecfe3e21782ac9cfe444e5
Piper McCorkle <piper@lutris.engineering>
2022-07-03 13:39:56 -0500

    Allow some scripts to fail to load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers"
-// import "@popperjs/core"
-import "bootstrap"
-// TODO: find a good way to defer running masonry until after image loading, while keeping it being triggerd by data-masonry
-import "masonry-layout"
+
+async function load() {
+    try {
+        await Promise.all([
+            import('controllers'),
+            // import "@popperjs/core"
+            import('bootstrap'),
+            // TODO: find a good way to defer running masonry until after image loading, while keeping it being triggerd by data-masonry
+            import('masonry-layout'),
+        ])
+    } catch (e) {
+        console.error('Failed to load a JS dependency', e)
+    }
+}
+
+load()
