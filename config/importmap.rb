7ccd8405830206c89d326271408dd98ed8bc4ebe
Piper McCorkle <piper@lutris.engineering>
2022-07-03 20:09:28 -0400

    Vendor JavaScript dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,6 @@ pin 'application', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'bootstrap', to: 'bootstrap.js', preload: true
-pin '@popperjs/core', to: 'https://unpkg.com/@popperjs/core@2.11.5/dist/esm/index.js', preload: true # @2.11.5
-pin 'masonry-layout', to: 'https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js', preload: true # @4.2.2
+pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+pin '@popperjs/core', to: 'popper.js', preload: true # @2.11.5
+pin 'masonry-layout', preload: true # @4.2.2
